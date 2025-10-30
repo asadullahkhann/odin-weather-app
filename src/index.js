bd9d5069ca5bbb27ebb02fb632f2d493fefcb2a1
Asad <asadullahkhan0098@gmail.com>
2025-10-31 01:04:20 +0530

    Refactor UI
    - move location info to the header
    - use one gradient for entire page
    - show mumbai weather on load

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import './styles.css';
+import loadingGif from './images/loading-loading-forever.gif';
+import searchSvg from './images/search.svg';
 import { getWeather, processWeather, getRequiredWeatherInfo } from './get-weather';
 import { renderWeatherInfo, convertToFahrenheit, convertToCelsius } from './dom-manipulator';
 import { getAppropriateGif } from './weather-gifs';
 
 const inputEl = document.querySelector('input');
 const enterBtn = document.querySelector('button');
-const loadingComponent = document.querySelector('.loading-component');
-const locationInfoDiv = document.querySelector('.location-info');
-const weatherInfoDiv = document.querySelector('.weather-info');
+const loadingComponent = document.querySelector('.error-card');
+const weatherInfoDiv = document.querySelector('.weather-card');
 const celsiusBtn = document.querySelector('.temp>button');
 const fahrenheitBtn = document.querySelectorAll('.temp>button')[1];
 const loadingComponentImages = loadingComponent.querySelectorAll('img');
@@ -15,36 +16,48 @@ const loadingComponentImages = loadingComponent.querySelectorAll('img');
 let requiredWeatherInfo;
 let location;
 
-enterBtn.addEventListener('click', async () => {
+const showWeather = async () => {
   if ((!inputEl.value) || (location === inputEl.value)) return;
   location = inputEl.value;
-  locationInfoDiv.setAttribute('class', 'location-info hide');
-  weatherInfoDiv.setAttribute('class', 'weather-info hide');
-  [...loadingComponentImages].slice(1).forEach(loadingComponentImage => {
+  const loadingImg = document.createElement('img');
+  loadingImg.src = loadingGif;
+  [...loadingComponentImages].forEach(loadingComponentImage => {
     loadingComponentImage.setAttribute('class', 'hide');
   });
-  loadingComponent.setAttribute('class', 'loading-component');
-  loadingComponentImages[0].setAttribute('class', '');
+  enterBtn.removeChild(enterBtn.firstChild);
+  enterBtn.appendChild(loadingImg);
+  enterBtn.removeEventListener('click', showWeather);
   const weather = await getWeather(location);
   if (weather === undefined) {
-    loadingComponentImages[0].setAttribute('class', 'hide');
-    loadingComponentImages[2].setAttribute('class', '');
+    weatherInfoDiv.setAttribute('class', 'hide')
+    loadingComponentImages[0].setAttribute('class', '');
+    loadingComponentImages[1].setAttribute('class', 'hide');
+    loadingComponent.setAttribute('class', 'error-card');
+    enterBtn.addEventListener('click', showWeather);
+    loadingImg.src = searchSvg;
   } else if (!weather.ok) {
+    weatherInfoDiv.setAttribute('class', 'hide');
     loadingComponentImages[0].setAttribute('class', 'hide');
     loadingComponentImages[1].setAttribute('class', '');
+    loadingComponent.setAttribute('class', 'error-card');
+    enterBtn.addEventListener('click', showWeather);
+    loadingImg.src = searchSvg;
   } else {
+    loadingImg.src = searchSvg;
+    enterBtn.addEventListener('click', showWeather);
     const processedWeather = await processWeather(weather);
     requiredWeatherInfo = getRequiredWeatherInfo(processedWeather);
     const gif = await getAppropriateGif(requiredWeatherInfo.icon);
     const gifURL = gif.data.images.original.url;
-    loadingComponent.setAttribute('class', 'loading-component hide');
-    locationInfoDiv.setAttribute('class', 'location-info');
+
     renderWeatherInfo(location, requiredWeatherInfo, gifURL);
-    weatherInfoDiv.setAttribute('class', 'weather-info');
+    weatherInfoDiv.setAttribute('class', 'weather-card');
     celsiusBtn.setAttribute('class', 'greyed-out');
     fahrenheitBtn.setAttribute('class', '');
   }
-});
+};
+
+enterBtn.addEventListener('click', showWeather);
 
 fahrenheitBtn.addEventListener('click', (e) => {
   convertToFahrenheit(e, requiredWeatherInfo);
@@ -53,3 +66,5 @@ fahrenheitBtn.addEventListener('click', (e) => {
 celsiusBtn.addEventListener('click', (e) => {
   convertToCelsius(e, requiredWeatherInfo);
 });
+
+window.addEventListener('load', showWeather);
