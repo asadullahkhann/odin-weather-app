d9c2ad7bd0bfc169ad42e53859101566c7cf2c49
Asad <asadullahkhan0098@gmail.com>
2026-06-04 17:59:47 +0530

    Clean up GIF logic and set fixed width on .col

--- src/weather-gifs.js
+++ src/weather-gifs.js
@@ -1,16 +1,6 @@
 async function getAppropriateGif(conditions) {
   conditions = conditions.toLowerCase();
-  conditions += ' weather sky'
-  console.log(conditions)
-  if (conditions.includes(',')) {
-    conditions = conditions.split(',')[0];
-  }
-  if (conditions.includes('night')) {
-    conditions += ' national geographic ';
-  } else if (conditions.includes('partially')) {
-    conditions = conditions.replace('partially', 'Partly');
-  }
-  console.log(conditions)
+  conditions += ' weather'
   const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=q7r5GoJBLCiQ2DtH8d7qLfjAIhWW79Ut&s=${conditions}`);
   const parsedResponse = await response.json();
   return parsedResponse;
